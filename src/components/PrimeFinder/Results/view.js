45065f7acf7ad8f5fb135c76c9512540b34be132
JonathanHalpern <jonnyhalpern@gmail.com>
2018-05-17 14:06:13 +0800

    Tidied css

--- src/components/PrimeFinder/Results/view.js
+++ src/components/PrimeFinder/Results/view.js
@@ -1,27 +1,50 @@
 import React from "react";
-import Typography from '@material-ui/core/Typography';
+import Typography from "@material-ui/core/Typography";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { withStyles } from "@material-ui/core/styles";
 
-const Results = ({ n, nthPrime, timeDifference, primeList, searchTime }) => (
-  <div>
+const styles = theme => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: 25,
+    width: "100%"
+  },
+  expansionPanel: {
+    marginTop: 25,
+    width: "100%"
+  },
+  primeList: {
+    textAlign: "left"
+  }
+});
+
+const Results = ({
+  n,
+  nthPrime,
+  timeDifference,
+  primeList,
+  searchTime,
+  classes
+}) => (
+  <div className={classes.container}>
     <Typography>
       prime {n} is <b>{nthPrime}</b>
     </Typography>
     <Typography>the search took: {searchTime} </Typography>
-    <ExpansionPanel>
+    <ExpansionPanel className={classes.expansionPanel}>
       <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
         <Typography>Full list of primes</Typography>
       </ExpansionPanelSummary>
       <ExpansionPanelDetails>
-        <Typography>
-          {primeList}
-          </Typography>
+          <Typography className={classes.primeList}>{primeList}</Typography>
       </ExpansionPanelDetails>
     </ExpansionPanel>
   </div>
 );
 
-export default Results;
+export default withStyles(styles)(Results);
